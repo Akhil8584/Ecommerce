d6fcc96bea204dd92e393051bead492d31f361ec
Akhil8584~ <akhiltv8584@gmail.com>
2023-08-08 19:44:47 +0530

    pattern

--- store/models.py
+++ store/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name=models.CharField(max_length=100)
+    descriptin=models.CharField(max_length=2000)
+    price=models.IntegerField()
+    image=models.ImageField(upload_to='images')
+    
--- store/views.py
+++ store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from .import views
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
+from .models import Product
 
 # Create your views here.
 def index(request):
@@ -9,7 +10,10 @@ def index(request):
 def about(request):
     return render(request,'about.html')
 def cycle(request):
-    return render(request,'cycle.html')
+    dict_cycle={
+        'cycle':Product.objects.all()
+    }
+    return render(request,'cycle.html',dict_cycle)
 def newsv(request):
     return render(request,'news.html')
 def contacts(request):
@@ -40,10 +44,11 @@ def register(request):
             return redirect('register')
         else:    
             user=User.objects.create_user(username=username,email=email,password=password)
-            user.save();
+            user.save()
             return redirect('/')
     else:
         return render(request,'register.html')
 def logout(request):
     auth.logout(request)
     return redirect('/')
+
